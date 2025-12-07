7dcc6c9f68c566af03fd765a2c11b1616249c42c
Severin <severin.amacher@gmx.net>
2025-12-07 13:27:38 +0100

    Implemented Day7 Part 2

--- 2025/Day7/Day7.cs
+++ 2025/Day7/Day7.cs
@@ -1,35 +1,87 @@
 using var sr = new StreamReader("./day7_input.txt");
-
+var lines = File.ReadLines("./day7_input.txt").ToArray();
 var beamIndexes = new List<int>();
-var line = sr.ReadLine();
-var totalSplits = 0;
-while (line is not null)
+var beamIndexHits = new Dictionary<int, long>();
+// var line = sr.ReadLine();
+var currentDepth = 0;
+foreach (var line in lines)
 {
     if (beamIndexes.Count() is 0)
     {
-        beamIndexes.Add(line.IndexOf('S'));
+        var startingIndex = line.IndexOf('S');
+        beamIndexes.Add(startingIndex);
+        beamIndexHits.Add(startingIndex, 1L);
     }
     else
     {
-        var addedIndexes = new HashSet<int>();
+        var addedIndexes = new Dictionary<int, long>();
         foreach (var beamIndex in beamIndexes)
         {
-            Console.WriteLine($"Checking Index: {beamIndex}");
+            // Console.WriteLine($"Checking Index: {beamIndex}");
             if (line[beamIndex] == '^')
             {
-                totalSplits++;
-                addedIndexes.Add(beamIndex - 1) ? 1 : 0;
-                addedIndexes.Add(beamIndex + 1) ? 1 : 0;
+                if (addedIndexes.TryGetValue(beamIndex - 1, out var nextLeft))
+                {
+                    addedIndexes[beamIndex - 1] = nextLeft + beamIndexHits[beamIndex];
+                }
+                else
+                {
+                    addedIndexes.Add(beamIndex - 1, beamIndexHits[beamIndex]);
+                }
+
+                if (addedIndexes.TryGetValue(beamIndex + 1, out var nextRight))
+                {
+                    addedIndexes[beamIndex + 1] = nextRight + beamIndexHits[beamIndex];
+                }
+                else
+                {
+                    addedIndexes.Add(beamIndex + 1, beamIndexHits[beamIndex]);
+                }
             }
             else
             {
-                addedIndexes.Add(beamIndex);
+                if (addedIndexes.TryGetValue(beamIndex, out var hits))
+                {
+                    addedIndexes[beamIndex] = beamIndexHits[beamIndex] + hits;
+                }
+                else
+                {
+                    addedIndexes.Add(beamIndex, beamIndexHits[beamIndex]);
+                }
             }
+
         }
-        beamIndexes = addedIndexes.ToList();
-        Console.WriteLine($"New Indexes: {string.Join(',', beamIndexes)}");
+        beamIndexHits = addedIndexes;
+        beamIndexes = addedIndexes.Keys.ToList();
     }
-    line = sr.ReadLine();
+    Visualize(lines, beamIndexHits, currentDepth);
+    // line = sr.ReadLine();
+    currentDepth++;
 }
 
-Console.WriteLine(totalSplits);
+var sum = 0L;
+foreach (var key in beamIndexHits.Keys)
+{
+    sum += beamIndexHits[key];
+}
+
+Console.WriteLine(sum);
+
+
+static void Visualize(string[] lines, Dictionary<int, long> beams, int currentDepth)
+{
+    var index = 0;
+    foreach (var character in lines[currentDepth])
+    {
+        if (beams.ContainsKey(index))
+        {
+            Console.Write(beams[index]);
+        }
+        else
+        {
+            Console.Write(character);
+        }
+        index++;
+    }
+    Console.Write(System.Environment.NewLine);
+}
